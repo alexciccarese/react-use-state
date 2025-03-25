57a097376cd1c9fdbd529bc7eb9b1fb3284e6cda
alexciccarese <alex.ciccarese@gmail.com>
2025-03-25 17:01:39 +0100

    add description display and button functionality in Main component

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,33 +1,48 @@
-import { useState } from "react"
-import languages from "./data/languages"
-
+import { useState } from "react";
+import languages from "./data/languages";
 
 export default function Main() {
+  const [selectedDescription, setSelectedDescription] = useState(""); 
+
+  const handleOpen = (description) => {
+    setSelectedDescription(description) 
+  };
+
   return (
-    <>
-      <div className="container mt-4 mb-4">
-        <h2>Learn Web development</h2>
+    <div className="container mt-4 mb-4">
+      <h2>Learn Web development</h2>
 
-        <div className="d-flex gap-3 mb-3">
+      <div className="d-flex gap-3 mb-3">
         {languages.map((item) => (
-          <Buttons key={`item-${item.id}`} name={item.name} />
+          <Buttons
+            key={`item-${item.id}`}
+            name={item.name}
+            handleOpen={() => handleOpen(item.description)} 
+          />
         ))}
-        </div>
-
-        <div className="text">
-          <p className="card-text">Seleziona un linguaggio per maggiori dettagli.</p>
-        </div>
       </div>
-    </>
+
+      <Content description={selectedDescription} /> 
+    </div>
   );
 }
 
-function Buttons({ name }) {
+function Buttons({ name, handleOpen, isSelected }) {
   return (
     <div className="buttons d-flex justify-content-start mt-3">
-      <button type="button" className="btn btn-primary">
+      <button type="button" 
+      className="btn btn-primary"
+      onClick={handleOpen}>
         {name}
       </button>
+    </div>
+  );
+}
+
+function Content({ description }) {
+  return (
+    <div className="text">
+      <p className="card-text">{description}</p>
     </div>
   );
 }
