8e57d28e708eaecb5d394e70aba8bb9ab9676911
alexciccarese <alex.ciccarese@gmail.com>
2025-03-25 16:01:57 +0100

    enhance Main component with language buttons and styling adjustments

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,12 +1,33 @@
 import { useState } from "react"
 import languages from "./data/languages"
+
+
 export default function Main() {
-  
   return (
     <>
-      <main>
-        Learn Web development
-      </main>
+      <div className="container mt-4 mb-4">
+        <h2>Learn Web development</h2>
+
+        <div className="d-flex gap-3 mb-3">
+        {languages.map((item) => (
+          <Buttons key={`item-${item.id}`} name={item.name} />
+        ))}
+        </div>
+
+        <div className="text">
+          <p className="card-text">Seleziona un linguaggio per maggiori dettagli.</p>
+        </div>
+      </div>
     </>
-  )
+  );
+}
+
+function Buttons({ name }) {
+  return (
+    <div className="buttons d-flex justify-content-start mt-3">
+      <button type="button" className="btn btn-primary">
+        {name}
+      </button>
+    </div>
+  );
 }
